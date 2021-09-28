37f301f4e863887c86916507c880de043bdecc37
Aron Faulhaber <aron.faulhaber@gmail.com>
2021-09-28 14:24:40 +0200

    basic 1 finished

--- Basic/script.js
+++ Basic/script.js
@@ -1,11 +1,24 @@
 function display(){
 
+
+
     let firstname = document.forms["form1"]["firstname"].value;
     let lastname = document.forms["form1"]["lastname"].value;
     let age = document.forms["form1"]["age"].value;
 
-    result=document.getElementById("display"); 
+    let len = firstname.length;
+    let text = document.getElementById("disp");
+    
+    if (len > 5) {
+        text.style.color = "red";
+    }
+    
+    else {
+        text.style.color = "green";
+    }
+    
+    text.innerHTML = 'My name is '+firstname+' '+lastname+' and I am '+age+' years old.';
 
-    result.innerHTML = 'My name is '+firstname+' '+lastname+' and I am '+age+' years old.'
 }
+
 
