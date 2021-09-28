1ea3821ac5bdaf0ad352577e0014d1d0b9804310
Aron Faulhaber <aron.faulhaber@gmail.com>
2021-09-28 13:53:02 +0200

    test

--- Basic/script.js
+++ Basic/script.js
@@ -4,7 +4,7 @@ function display(){
     let lastname = document.forms["form1"]["lastname"].value;
     let age = document.forms["form1"]["age"].value;
 
-    result = document.getElementById("display"); 
+    result=document.getElementById("display"); 
 
     result.innerHTML = 'My name is '+firstname+' '+lastname+' and I am '+age+' years old.'
 }
